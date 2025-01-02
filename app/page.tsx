efd4ca8312bb9724a1f07776d1030e8108884c15
seifeldin15 <seifeldin02@outlook.com>
2025-01-02 02:07:27 +0200

    remove header

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,9 @@ export default function Home() {
   return (
     <div className="">
       <div className="w-full flex justify-center relative pointer-events-none">
-        {/* header big shadow */}
-        <div className="absolute rounded-full top-[-1500px] h-[2000px] w-full max-w-[1500px] top-gradient-shadow opacity-50 sm:opacity-100 z-20"></div>
       </div>
       <Header />
-      <Hero />
-      <OurFeatures />
-      <ServiceView />
-      <Pricing />
-      <RepeatedCta />
-      <Footer />
+     
     </div>
   );
 }
