522be6da1f164ac918ce0927104a02bd3d0a87b7
seifeldin15 <seifeldin02@outlook.com>
2025-01-08 21:12:53 +0200

    reseller page

---
+++ app/reseller/page.tsx
@@ -0,0 +1,16 @@
+import Hero from '@/components/reseller/Hero'
+import Features from '@/components/reseller/Features'
+import Join from '@/components/reseller/Join'
+import Features2 from '@/components/reseller/Features2'
+import GetStarted from '@/components/reseller/GetStarted'
+export default function ResellerPage() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+      <Join />
+      <Features2 />
+      <GetStarted />
+    </main>
+  )
+}
