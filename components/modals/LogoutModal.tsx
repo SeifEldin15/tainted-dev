e30aa39b5851df857a8eafe011ca809daa2caeb1
seifeldin15 <seifeldin02@outlook.com>
2025-01-22 20:39:42 +0200

    Logout modal redesign

--- components/modals/LogoutModal.tsx
+++ components/modals/LogoutModal.tsx
@@ -20,15 +20,22 @@ export function LogoutModal() {
 
   return (
     <Dialog open={currentModal.modalName === "logoutModal"}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] bg-white/95 backdrop-blur-sm border-0 shadow-lg">
         <DialogHeader>
-          <DialogTitle>{`Action Required! You are about to logout!`}</DialogTitle>
-          <DialogDescription>
-            {`Are you sure you want to logout? You will be redirected to the login page.`}
+          <DialogTitle className="text-black">
+            Action Required! <span className="text-brand">You are about to logout!</span>
+          </DialogTitle>
+          <DialogDescription className="text-gray-600">
+            Are you sure you want to logout? You will be redirected to the login page.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button variant={"ghost"} onClick={() => handleClose()} type="submit">
+          <Button 
+            variant={"ghost"} 
+            onClick={() => handleClose()} 
+            type="submit"
+            className="text-black hover:text-black/80 hover:bg-transparent border-1 border-black"
+          >
             Cancel
           </Button>
           <Button
@@ -40,6 +47,7 @@ export function LogoutModal() {
             }
             variant={"attention"}
             type="submit"
+            className="bg-brand hover:bg-brand/90 text-white"
           >
             Logout
           </Button>
