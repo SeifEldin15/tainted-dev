a73a4388be549a65656d06001f991ca968fe1bc4
seifeldin15 <seifeldin02@outlook.com>
2025-01-27 05:29:06 +0200

    import popup

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,31 @@ import ServiceView from "@/components/landing/ServiceView";
 import Footer from "@/components/landing/footer";
 import OurFeatures from "@/components/landing/ourfeatures";
 import RepeatedCta from "@/components/landing/repeatedcta";
+import { BottomBanner } from "@/components/Popup"
 
 export default function Home() {
   return (
-    <div className="dark:bg-gray-900">
-      <div className="w-full flex justify-center relative pointer-events-none">
+    <>
+      <div className="dark:bg-gray-900">
+        <div className="w-full flex justify-center relative pointer-events-none">
+        </div>
+        <Header />
+        <Hero />
+        <div id="features" className="dark:bg-gray-900">
+          <Features />
+        </div>
+        <div id="pricing" className="dark:bg-gray-900">
+          <Pricing />
+        </div>
+        <Countries />
+        <Popular />
+        <div id="about" className="dark:bg-gray-900">
+          <About />
+        </div>
+        <FAQ />
+        <Footer />
       </div>
-      <Header />
-      <Hero />
-      <div id="features" className="dark:bg-gray-900">
-        <Features />
-      </div>
-      <div id="pricing" className="dark:bg-gray-900">
-        <Pricing />
-      </div>
-      <Countries />
-      <Popular />
-      <div id="about" className="dark:bg-gray-900">
-        <About />
-      </div>
-      <FAQ />
-      <Footer />
-    </div>
+      <BottomBanner />
+    </>
   );
 }
